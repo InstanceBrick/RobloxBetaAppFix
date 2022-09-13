197b54ce38118a60f0552558c55e69f410e56666
InstanceBrick <94509043+InstanceBrick@users.noreply.github.com>
2022-09-13 18:19:38 -0400

    Update FixBetaApp.ps1

--- Source/FixBetaApp.ps1
+++ Source/FixBetaApp.ps1
@@ -9,9 +9,25 @@ $balloon.Visible = $true
 $balloon.ShowBalloonTip(7000)
 
 
+
+
+
+
+
 $Key = "HKCU:\Software\ROBLOX Corporation\Environments\roblox-player"
 while ($true)
 {
+
+$roblox = Get-Process RobloxPlayerBeta -ErrorAction SilentlyContinue
+#Basically we are getting RobloxPlayerBeta and checking if its running, then if it starts to not respond we close the program
+if ($roblox) {
+    if((get-process RobloxPlayerBeta).Responding){
+     #Write-Output "Roblox Running Properly"
+    }else{
+      #Write-Output "Roblox Crash"
+        Stop-Process -Name RobloxPlayerBeta -Force
+    }
+}
     $CurrentValue = (Get-ItemProperty -Path $Key -Name "LaunchExp").LaunchExp
     if ($CurrentValue -eq "InApp")
     {
