361516cbd79e0cd1ad3515bc477b3cb4b7a8178b
InstanceBrick <94509043+InstanceBrick@users.noreply.github.com>
2022-09-15 18:53:16 -0400

    Update FixBetaApp.ps1

--- Source/FixBetaApp.ps1
+++ Source/FixBetaApp.ps1
@@ -1,29 +1,23 @@
 # THIS IS MEANT TO RUN THROUGH AN EXE FILE (MAY BREAK IF YOU RUN THE RAW SCRIPT)
 # You can compile this script using ps2exe or download my pre-compiled version in releases!
 # https://github.com/InstanceBrick/RobloxBetaAppFix/releases/tag/v2.0.0up - Auto updater
-
-
-# Check for multiple instances of the current program (stops errors/overlapping code)
-
-function numInstances([string]$process)
-{
+$Global:IsEnabled = $true
+function numInstances([string]$process) {
     @(get-process -ea silentlycontinue $process).count
 }
-if ((numInstances FixBetaApp) -gt 1) {
 Add-Type -AssemblyName System.Windows.Forms 
-$global:balloon = New-Object System.Windows.Forms.NotifyIcon
-$path = (Get-Process -id $pid).Path
-$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
-$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Error 
-$balloon.BalloonTipText = 'Multiple instances opened (Please only open 1 instance)'
-$balloon.BalloonTipTitle = "Error!" 
-$balloon.Visible = $true 
-$balloon.ShowBalloonTip(7000)
+if ((numInstances FixBetaApp) -gt 1) {
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+    $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Error 
+    $balloon.BalloonTipText = 'Multiple instances opened (Please only open 1 instance)'
+    $balloon.BalloonTipTitle = "Error!" 
+    $balloon.Visible = $true 
+    $balloon.ShowBalloonTip(1000)
     Stop-Process -Name FixBetaApp -Force
 }
-
-
-
+ 
 $global:balloon = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
@@ -31,16 +25,16 @@ $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::None
 $balloon.BalloonTipText = 'RobloxBetaFix has Loaded!'
 $balloon.BalloonTipTitle = "Welcome!" 
 $balloon.Visible = $true 
-$balloon.ShowBalloonTip(7000)
+$balloon.ShowBalloonTip(1000)
 
 
-if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript")
-{ # Powershell script
-	$ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    # Powershell script
+    $ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 }
-else
-{ # PS2EXE compiled script
-	$ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+else {
+    # PS2EXE compiled script
+    $ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
 }
 
 
@@ -57,7 +51,7 @@ $icon = [System.Drawing.Icon]::ExtractAssociatedIcon("$ScriptPath/FixBetaApp.exe
  
 
 $Main_Tool_Icon = New-Object System.Windows.Forms.NotifyIcon
-$Main_Tool_Icon.Text = "RobloxBetaFix"
+$Main_Tool_Icon.Text = "FixBetaApp"
 $Main_Tool_Icon.Icon = $icon
 $Main_Tool_Icon.Visible = $true
 
@@ -70,6 +64,10 @@ $Menu_Exit.Text = "Exit"
 $Menu_Kill = New-Object System.Windows.Forms.MenuItem
 $Menu_Kill.Text = "Kill Roblox"
 
+$Menu_Toggle = New-Object System.Windows.Forms.MenuItem
+$Menu_Toggle.Text = "Disable"
+
+
 $Menu_AttachedProcesses = New-Object System.Windows.Forms.MenuItem
 $Menu_AttachedProcesses.Enabled = $false
 $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlayerBeta)
@@ -77,102 +75,110 @@ $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlay
 $contextmenu = New-Object System.Windows.Forms.ContextMenu
 $Main_Tool_Icon.ContextMenu = $contextmenu
 $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_AttachedProcesses)
+$Main_Tool_Icon.contextMenu.MenuItems.Add("-")
+$Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Toggle)
 $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Kill)
 $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Exit)
 
 
  
 $Main_Tool_Icon.Add_Click({                    
-    If ($_.Button -eq [Windows.Forms.MouseButtons]::Left) {
-        $Main_Tool_Icon.GetType().GetMethod("ShowContextMenu",[System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic).Invoke($Main_Tool_Icon,$null)
-        #Refresh ContextMenu
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_AttachedProcesses)
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_Kill)
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_Exit)
-        Start-Sleep -Milliseconds 1
-        $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlayerBeta)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_AttachedProcesses)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Kill)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Exit)
-    }
-})
+        If ($_.Button -eq [Windows.Forms.MouseButtons]::Left) {
+            $Main_Tool_Icon.GetType().GetMethod("ShowContextMenu", [System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic).Invoke($Main_Tool_Icon, $null)
+            #Refresh ContextMenu
+      
+            Start-Sleep -Milliseconds 1
+            $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlayerBeta)
+       
+        }
+    })
 
 $Main_Tool_Icon.Add_Click({                    
-    If ($_.Button -eq [Windows.Forms.MouseButtons]::Right) {
-        $Main_Tool_Icon.GetType().GetMethod("ShowContextMenu",[System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic).Invoke($Main_Tool_Icon,$null)
-        #Refresh ContextMenu
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_AttachedProcesses)
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_Kill)
-        $Main_Tool_Icon.contextMenu.MenuItems.Remove($Menu_Exit)
-        Start-Sleep -Milliseconds 1
-        $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlayerBeta)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_AttachedProcesses)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Kill)
-        $Main_Tool_Icon.contextMenu.MenuItems.Add($Menu_Exit)
-    }
-})
-
- 
+        If ($_.Button -eq [Windows.Forms.MouseButtons]::Right) {
+            $Main_Tool_Icon.GetType().GetMethod("ShowContextMenu", [System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic).Invoke($Main_Tool_Icon, $null)
+            #Refresh ContextMenu
+     
+            Start-Sleep -Milliseconds 1
+            $Menu_AttachedProcesses.Text = "Attached Processes: " + (numInstances RobloxPlayerBeta)
+       
+        }
+    })
 
 
 
 $Menu_Exit.add_Click({
-    $Main_Tool_Icon.Visible = $false
-	Stop-Job -Name "Loop"
-	$global:balloon = New-Object System.Windows.Forms.NotifyIcon
-	$path = (Get-Process -id $pid).Path
-	$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
-	$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::None 
-	$balloon.BalloonTipText = 'RobloxBetaFix has closed!'
-	$balloon.BalloonTipTitle = "Goodbye!" 
-	$balloon.Visible = $true 
-	$balloon.ShowBalloonTip(7000)
-    Stop-Process -Name FixBetaApp -Force
- })
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+    $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::None 
+    $balloon.BalloonTipText = 'RobloxBetaFix has closed!'
+    $balloon.BalloonTipTitle = "Goodbye!" 
+    $balloon.Visible = $true 
+    $balloon.ShowBalloonTip(1000)
+        $Main_Tool_Icon.Visible = $false
+        $Main_Tool_Icon.Dispose();
+        Stop-Job -Name "Loop"
+
+
+        Stop-Process -Name FixBetaApp -Force
+
+    })
  
- $Menu_Kill.add_Click({
-    $roblox = Get-Process RobloxPlayerBeta -ErrorAction SilentlyContinue
-    if ($roblox) {
+$Menu_Kill.add_Click({
+        $roblox = Get-Process RobloxPlayerBeta -ErrorAction SilentlyContinue
+        if ($roblox) {
             Stop-Process -Name RobloxPlayerBeta -Force
         }
- })
+    })
  
-
- $WhLoop = {
+   
+$WhLoop = {
     $Key = "HKCU:\Software\ROBLOX Corporation\Environments\roblox-player"
     while ($true) {
-       
-    $roblox = Get-Process RobloxPlayerBeta -ErrorAction SilentlyContinue
-    #Basically we are getting RobloxPlayerBeta and checking if its running, then if it starts to not respond we close the program
-    if ($roblox) {
-        if((get-process RobloxPlayerBeta).Responding){
-         #Write-Output "Roblox Running Properly"
-            }else{
-            #Write-Output "Roblox Crash"
-                Get-Process RobloxPlayerBeta|
-                Sort-Object StartTime -Descending|
-                Select-Object -Skip 1|
-                Stop-Process
-            #Stop-Process -Name RobloxPlayerBeta -Force
+        
+            $roblox = Get-Process RobloxPlayerBeta -ErrorAction SilentlyContinue
+            #Basically we are getting RobloxPlayerBeta and checking if its running, then if it starts to not respond we close the program
+            if ($roblox) {
+                if ((get-process RobloxPlayerBeta).Responding) {
+                    #Write-Output "Roblox Running Properly"
+                }
+                else {
+                    #Write-Output "Roblox Crash"
+                    Get-Process RobloxPlayerBeta |
+                    Sort-Object StartTime -Descending |
+                    Select-Object -Skip 1 |
+                    Stop-Process
+                    #Stop-Process -Name RobloxPlayerBeta -Force
+                }
             }
-        }
-        $CurrentValue = (Get-ItemProperty -Path $Key -Name "LaunchExp").LaunchExp
-        if ($CurrentValue -eq "InApp")
-        {
-            #Write-Output "Value is InApp."
-            Set-ItemProperty -Path $Key -Name "LaunchExp" -Value "InBrowser"
-        }
-    
+            $CurrentValue = (Get-ItemProperty -Path $Key -Name "LaunchExp").LaunchExp
+            if ($CurrentValue -eq "InApp") {
+                #Write-Output "Value is InApp."
+                Set-ItemProperty -Path $Key -Name "LaunchExp" -Value "InBrowser"
+            }
+       Write-Output $Menu_Toggle.Text
         Start-Sleep -Milliseconds 20
     }
 }
-Start-Job -ScriptBlock $WhLoop -Name "Loop"
 
+$Menu_Toggle.add_Click({
+    if ($Menu_Toggle.Text -eq "Disable" ) {
+        $Menu_Toggle.Text = "Enable"
+        Stop-Job -Name "Loop"
+   } else {
+        $Menu_Toggle.Text = "Disable"  
+        Start-Job -ScriptBlock $WhLoop -Name "Loop"
+    }
+})
+ 
+
+Start-Job -ScriptBlock $WhLoop -Name "Loop"
 
 
 [System.GC]::Collect()
 
- 
+ Start-Sleep -Seconds 1 
+ $global:balloon.Dispose()
 
 # Create an application context for it to all run within.
 # This helps with responsiveness, especially when clicking Exit.
